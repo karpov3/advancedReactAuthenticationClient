cd31f1f8e3e34af321f5f87d8fcbb0646a76e5f9
Alexander Karpov <alexander.karpov@neosperience.com>
2017-08-31 11:18:27 +0200

    Lesson 90

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './header';
-
+import Signin from './auth/signin';
 export default class App extends Component {
   render() {
     return (
       <div>
+      <Signin />
       <Header />
+
       React simple starter</div>
     );
   }
